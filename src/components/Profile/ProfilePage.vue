6c2bcafefb62840ea46c5fd744c7a63dcb28825e
Naoto <nstm.mrl1119@gmail.com>
2020-12-07 04:46:02 -0800

    Added components for login/signup/signupform/basicprofilepage

---
+++ src/components/Profile/ProfilePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <ProfileImage class="profile-image" />
+    <TwitterLink />
+  </div>
+</template>
+
+<script>
+import ProfileImage from "./ProfileImage.vue";
+import TwitterLink from "./TwitterLink.vue";
+
+export default {
+  name: "profile page",
+  components: {
+    ProfileImage,
+    TwitterLink,
+  },
+};
+</script>
+
+<style>
+.profile-image {
+  margin-bottom: 10px;
+}
+</style>
