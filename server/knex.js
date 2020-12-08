e31cd387d36a253d1ea18102ff2bea4d3b647b3f
Naoto <nstm.mrl1119@gmail.com>
2020-12-08 07:55:40 -0800

    modified knex config

--- server/knex.js
+++ server/knex.js
@@ -1,11 +1,23 @@
 const knex = require("knex");
 require("dotenv").config();
 
+const db = knex({
+  client: "pg",
+  connection:
+    process.env.DATABASE_URL ||
+    `postgres://${process.env.USER}:${process.env.PASSWORD}@5432/sell_yourself`,
+  searchPath: "public",
+});
+
 // const db = knex({
 //   client: "pg",
-//   connection:
-//     process.env.DATABASE_URL ||
-//     `postgres://${process.env.USER}:${process.env.PASSWORD}@5432/sell_yourself`,
+//   connection: {
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     charset: "utf8",
+//   },
 //   searchPath: "public",
 //   migrations: {
 //     directory: "./migrations",
@@ -14,23 +26,5 @@ require("dotenv").config();
 //     directory: "./data",
 //   },
 // });
-
-const db = knex({
-  client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    charset: "utf8",
-  },
-  searchPath: "public",
-  migrations: {
-    directory: "./migrations",
-  },
-  seeds: {
-    directory: "./data",
-  },
-});
 
 module.exports = db;
