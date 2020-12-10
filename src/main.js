26a2049b617c8bbd48de67cff290f35f067c0e79
Naoto <nstm.mrl1119@gmail.com>
2020-12-10 00:47:15 -0800

    editted fbconfig

--- src/main.js
+++ src/main.js
@@ -13,13 +13,29 @@ Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.component("Tweet", Tweet);
 
+const apiKey = process.env.FB_APIKEY;
+const authDomain = process.env.FB_AUTHDOMAIN;
+const projectId = process.env.FB_PROJECTID;
+const storageBucket = process.env.FB_STORAGEBUCKET;
+const messagingSenderId = process.env.FB_MESSAGINGSENDERID;
+const appId = process.env.FB_APPID;
+
+console.log(
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId
+);
+
 const firebaseConfig = {
-  apiKey: process.env.FB_APIKEY,
-  authDomain: process.env.FB_AUTHDOMAIN,
-  projectId: process.env.FB_PROJECTID,
-  storageBucket: process.env.FB_STORAGEBUCKET,
-  messagingSenderId: process.env.FB_MESSAGINGSENDERID,
-  appId: process.env.FB_APPID,
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
 };
 
 firebase.initializeApp(firebaseConfig);
